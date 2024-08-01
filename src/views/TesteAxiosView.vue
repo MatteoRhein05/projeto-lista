98af929c071db0f1d8a4459dbb00ce285febab44
Matteo Miguel Rhein <matteo2005rh@gmail.com>
2024-08-01 14:53:50 -0300

    UPDATE

--- src/views/TesteAxiosView.vue
+++ src/views/TesteAxiosView.vue
@@ -1,12 +1,26 @@
 <template>
+    <div>
     <p>teste do AXIOS</p>
+    <button @click="buscarEstados">testar!!</button>
+    <p v-for="estado in listaEstados" :key="estado.id">{{ estado.nome }}</p>
+    </div>
 </template>
 
 <script>
+import axios from "axios";
     export default{
         data(){
             return{
-                
+                listaEstados:[]
+            }
+        },
+        methods:{
+            async buscarEstados(){
+                //buscar a lista de estados no servidor
+                //http://localhost:8080/estados
+                const response = await axios.get("http://localhost:8080/estados");
+                console.log(response.data);
+                this.listaEstados = response.data;
             }
         }
     
