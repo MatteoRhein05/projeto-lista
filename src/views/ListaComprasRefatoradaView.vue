f59767427ace8f2a0630ab33d9b50ff0c2e21eae
Matteo Miguel Rhein <matteo2005rh@gmail.com>
2024-07-31 22:50:27 -0300

    UPDATE

---
+++ src/views/ListaComprasRefatoradaView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ListaCompraRefatorada from '../components/ListaCompraRefatorada.vue'
+</script>
+
+<template>
+  <main>
+    <ListaCompraRefatorada />
+  </main>
+</template>
+
