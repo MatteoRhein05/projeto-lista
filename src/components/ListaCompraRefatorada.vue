95b473f855306e34601f8499c040b5b470d42b54
Matteo Miguel Rhein <matteo2005rh@gmail.com>
2024-08-01 10:02:41 -0300

    UPDATE

--- src/components/ListaCompraRefatorada.vue
+++ src/components/ListaCompraRefatorada.vue
@@ -1,15 +1,7 @@
 
 <template>
     <div class="container">
-        <h1>Lista de compras</h1>
-        <hr/>
-        <p><input type="text" v-model="produto" placeholder="Nome do produto" class="form-control" /></p>
-        <p><input type="text" v-model="quantidade" placeholder="Quantidade do produto" class="form-control" /></p>
-        <div v-if="isInvalido" class="alert alert-danger" role="alert">
-             Nome do produto e quantidade devem ser preenchidos!!!
-        </div>
-        <button type="submit" v-on:click.prevent="incluirProduto" class="btn btn-primary">Incluir</button>
-        <hr/>
+       <FormCompraRefatorada v-on:novo-produto="incluirProduto"/> 
         <div class="list-group">
             <div class="list-group-item" v-for="(item, index) in lista" v-bind:key="index">
                 <span><strong>{{item.produto}}</strong></span>
@@ -22,34 +14,21 @@
     </div>
 </template>
 
-<script> 
+<script>
+import FormCompraRefatorada from './FormCompraRefatorada.vue';
    export default{
+    components:{
+        FormCompraRefatorada,
+    },
         data(){
             return {
-                produto:'',
-                quantidade:'',
-                lista: [],
-                isInvalido:false,
-
+                lista: []
             }
         },
         methods:{
-            incluirProduto(){
-               console.log(this.produto , this.quantidade);
-               if(this.produto ==='' || this.quantidade===''){
-                 this.isInvalido = true;
-                  return;
-               }
-               this.isInvalido = false;
-
-
-               this.lista.push({
-                produto:this.produto,
-                quantidade:this.quantidade
-
-                 });
-                 this.produto = '';
-                 this.quantidade = '';
+            incluirProduto(valor){
+                this.lista.push(valor);
+               
             },
             excluirProduto(posicao){
                 console.log(posicao);
